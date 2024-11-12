389672657f093b0f327520349229e9fd4019d7cb
wanglei <806416060@qq.com>
2024-11-12 15:01:01 +0800

    优化选择排序

--- src/pages/Sort/index.tsx
+++ src/pages/Sort/index.tsx
@@ -28,6 +28,28 @@ const Index = () => {
     return arr;
 }
 
+/**
+ * 检查数组是否按升序排序
+ *
+ * 此函数通过比较数组中相邻的元素，判断整个数组是否按照升序排列
+ * 它从数组的第二个元素开始遍历，如果发现任何一对相邻元素不满足前一个元素小于等于后一个元素的条件，
+ * 则认为数组没有按升序排序
+ *
+ * @param arr 待检查的数组
+ * @returns 如果数组按升序排序，则返回true；否则返回false
+ */
+const sortHelp = (arr: any[]) => {
+    for (let i = 1; i < arr.length; i++){
+        // 如果当前元素小于前一个元素，则数组不是升序排序
+        if(arr[i-1] > arr[i]){
+            return false
+        }
+        // 如果没有发现逆序的情况，则认为数组是升序排序
+        return true
+    }
+}
+
+
     return (
         <div>
             <div>选择排序：</div>
@@ -36,6 +58,8 @@ const Index = () => {
             <br />
             字符串：{JSON.stringify(strArr)}
             结果：{JSON.stringify(chooseSort<string>(strArr))}
+            <br />
+            检查排序结果：{JSON.stringify(sortHelp(chooseSort<number>(data)))}
         </div>
     );
 };
