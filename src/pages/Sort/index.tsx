0bc1efe9a0d419f5d8772937290410805e3096d1
wanglei <806416060@qq.com>
2024-11-12 14:41:54 +0800

    优化选择排序

--- src/pages/Sort/index.tsx
+++ src/pages/Sort/index.tsx
@@ -5,24 +5,29 @@ const Index = () => {
     const data = [5,65,1,2,3,4,6,77]
     const strArr = ['123123','sdfsdfsdf', '1ssrer', 'sssss']
 
-    const chooseSort = <T,>(arr: T[]): T[] => {
-        for (let i = 0; i < arr.length; i++) {
-            // 选择arr[i,n)最小值的索引
-            let minIndex = i
-            for (let j = i; j < arr.length; j++) {
-                if(arr[j] < arr[minIndex]){
-                    minIndex = j
-                }
-            }
-            // arr[i]和arr[minIndex] 交换位置
-            let temp = arr[i]
-            arr[i] = arr[minIndex]
-            arr[minIndex] = temp
-
-        }
-        return arr
-
+    const chooseSort = <T extends number | string>(arr: T[]): T[] => {
+    if (!Array.isArray(arr)) {
+        throw new Error('Input must be an array');
     }
+
+    for (let i = 0; i < arr.length; i++) {
+        // 选择arr[i,n)最小值的索引
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // 只在找到最小值后进行一次交换
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    return arr;
+}
+
     return (
         <div>
             <div>选择排序：</div>
